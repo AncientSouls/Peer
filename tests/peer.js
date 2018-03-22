33491bd2bfa6ad9df907b57b381a248da796884d
Ivan S Glazunov <ivansglazunov@gmail.com>
2018-03-22 20:54:52 +0300

    0.1.5
    not needed connect, valentine was right

--- tests/peer.js
+++ tests/peer.js
@@ -2,13 +2,17 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const _ = require("lodash");
 const chai_1 = require("chai");
+const create_local_transport_1 = require("ancient-channels/lib/create-local-transport");
 const peer_1 = require("../lib/peer");
 function default_1() {
     describe('Peer:', () => {
         it('connect() / peer.exec() / cursor.exec() / channel.destroy()', () => {
             const peer1 = new peer_1.Peer();
             const peer2 = new peer_1.Peer();
-            const channelId = peer1.connect(peer2);
+            const localChannel = peer1.channelsManager.create();
+            const remoteChannel = peer2.channelsManager.create();
+            create_local_transport_1.default(localChannel, remoteChannel);
+            const channelId = localChannel.id;
             const cursor = peer1.cursorsManager.create();
             cursor.exec({ channelId, apiQuery: null, query: 1 });
             chai_1.assert.equal(cursor.data, 1);
